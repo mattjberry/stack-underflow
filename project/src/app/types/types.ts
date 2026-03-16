59f4eaffb38a773c2bc9afc82a92db47208ee28b
Matt Berry <matthewjberry03@gmail.com>
2026-03-16 12:14:47 -0600

    Added Reply and Attachment types modified Posts to iunclude a field for total votes

--- project/src/app/types/types.ts
+++ project/src/app/types/types.ts
@@ -18,4 +18,27 @@ export type Post = {
   body: string;
   created_at: Date;
   reply_count: number;
+  vote_score: number;
 };
+
+// Reply
+export type Reply = {
+  id: number;
+  post_id: number;
+  parent_reply_id: number;
+  author_id: number;
+  author_name: string;
+  body: string;
+  created_at: Date;
+  vote_score: number;
+}
+
+// Attachments
+export type Attachment = {
+  id: number;
+  target_type: string;
+  target_id: number;
+  mime_type: string;
+  size_bytes: number;
+  file_path: string;
+}
