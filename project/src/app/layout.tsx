e7e874d208eb79415bd6ae480f04ef5be847949e
Matt Berry <matthewjberry03@gmail.com>
2026-03-18 20:06:00 -0600

    added signout button when signed in

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Breadcrumbs from "./components/Breadcrumbs";
 import { SessionProvider } from "next-auth/react";
+import { redirect } from "next/dist/server/api-utils";
+import { auth } from "@/lib/auth";
+import { signOut } from "@/lib/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,16 +22,31 @@ export const metadata: Metadata = {
   description: "Stack Overflow clone created for CMPT353 - Full Stack Development",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await auth();
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <SessionProvider>
-          <Breadcrumbs />
+          <nav>
+            <Breadcrumbs />
+            {session ? (
+              <form action={async () => {
+                "use server";
+                await signOut({ redirectTo: "/" });
+              }}>
+                <span>Signed in as {session.user.name}</span>
+                <button type="submit">Sign Out</button>
+              </form>
+            ) : null}
+          </nav>
+          
           {children}
         </SessionProvider>
       </body>
